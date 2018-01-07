74633ec4beb92a8f2631a6703f84d1628f1a9e6c
Jack Kyle <jackakyle@gmail.com>
2018-01-08 00:30:01 +0100

    fixed P_Map10x10_Init not being defined in the .h file

--- Battleships/source/P_Map10x10.h
+++ Battleships/source/P_Map10x10.h
@@ -36,4 +36,5 @@ void P_Map10x10_configureBG3();
 void SetMap10x10To(int index10, int tileIndex);
 void SetSubMap10x10To(int index10, int tileIndex);
 int getIndex(int x, int y);
+void P_Map10x10_Init( int cols, int rows );
 
