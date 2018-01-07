8fa4f074a1b083ed31ec10a7203f05c5f59793f2
Jack Kyle <jackakyle@gmail.com>
2018-01-07 23:13:56 +0100

    Added .gitignore, removed build files from tracking

--- Battleships/build/Ship.d
+++
@@ -1,4 +0,0 @@
-Ship.o: /home/nds/files/workspace/Battleships/Battleships/source/Ship.c \
-  /home/nds/files/workspace/Battleships/Battleships/source/Ship.h
-
-/home/nds/files/workspace/Battleships/Battleships/source/Ship.h:
